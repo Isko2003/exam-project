7c463113b1da67108e2a6defb19a49e65cbeecdd
Isko2003 <theazeisko2003@gmail.com>
2025-03-25 12:18:36 +0400

    access to login page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+      <Link to={"/login"}>
+        <a href="" className="underline text-blue-400">
+          Go to Login Page
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
